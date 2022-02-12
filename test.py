f0058c82b377b7760881342ad441987d6b2138df
nicstim <sashapskov60@gmail.com>
2022-02-12 10:20:26 +0300

    comment

--- test.py
+++ test.py
@@ -1,24 +1,30 @@
-def func(x):
-    print(x)
+a = 45670
+# a = 7863121
+# a = 7863121
+# ../dist/configure --disable-shared --enable-cxx --with-pic --prefix=BDB_PREFIX
+# Input: 01
+# Output: 1 0
+#
+# Input: 45670
+# Output: 12 10
+#
+# Input: 7863121
+# Output: 12 16
+#
+# Input: 77620
+# Output: 14 8
+#
+# Input: 1357
+# Output: 16 0
+#
+# Input: 240664826606
+# Output: 0 50
+count = len(str(a))
+
+total = sum([int(item) for item in str(a)])
+
+print(total, count)
+print(total/count)
 
 
-def create_handlers(callback):
-    handlers = list()
-    # for step in range(5):
-    #     handlers.append(lambda: callback(step))
-    f = lambda x: callback(x)
-    for step in range(5):
-        handlers.append(f(step))
-    # handlers = [f(x) for x in range(5)]
-    return handlers
-
-
-def execute_handlers(handlers):
-    for handler in handlers:
-        handler()
-
-
-a = create_handlers(func)
-print(a)
-# execute_handlers(a)
 
